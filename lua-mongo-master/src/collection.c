05f7180cbc03f2b892d6b15c820e59158bdbdac5
nneesshh <nneesshh@163.com>
2019-05-30 10:58:49 +0800

    add RETURN_NEW flag to findAndModify

--- lua-mongo-master/src/collection.c
+++ lua-mongo-master/src/collection.c
@@ -75,6 +75,8 @@ static int m_findAndModify(lua_State *L) {
 	int nres = 1;
 	mongoc_find_and_modify_opts_t *opts = mongoc_find_and_modify_opts_new();
 	mongoc_find_and_modify_opts_append(opts, options);
+	/* Create the document if it didn't exist, and return the updated document */
+	mongoc_find_and_modify_opts_set_flags(opts, MONGOC_FIND_AND_MODIFY_UPSERT | MONGOC_FIND_AND_MODIFY_RETURN_NEW);
 	if (!mongoc_collection_find_and_modify_with_opts(collection, query, opts, &reply, &error)) nres = commandError(L, &error);
 	else pushBSONField(L, &reply, "value", false);
 	mongoc_find_and_modify_opts_destroy(opts);
